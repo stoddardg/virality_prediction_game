0801ce54f6957ceea22436140b2b286b1f577644
Greg Stoddard <stoddardg@gmail.com>
2015-10-21 15:28:26 -0400

    Changed to different lightbox js package

--- app/static/script.js
+++ app/static/script.js
@@ -1,30 +1,30 @@
-$(function() {
-     $('#image_1').on('click',function(){
-        var src = $(this).attr('src');
-        var img = '<img src="' + src + '" class="img-responsive"/>';
-        $('#myModal').modal();
-        $('#myModal').on('shown.bs.modal', function(){
-            $('#myModal .modal-body').html(img);
-        });
-        $('#myModal').on('hidden.bs.modal', function(){
-            $('#myModal .modal-body').html('');
-        });
-   });
-});
+// $(function() {
+//      $('#image_1').on('click',function(){
+//         var src = $(this).attr('src');
+//         var img = '<img src="' + src + '" class="img-responsive"/>';
+//         $('#myModal').modal();
+//         $('#myModal').on('shown.bs.modal', function(){
+//             $('#myModal .modal-body').html(img);
+//         });
+//         $('#myModal').on('hidden.bs.modal', function(){
+//             $('#myModal .modal-body').html('');
+//         });
+//    });
+// });
 
-$(function() {
-     $('#image_2').on('click',function(){
-        var src = $(this).attr('src');
-        var img = '<img src="' + src + '" class="img-responsive"/>';
-        $('#myModal').modal();
-        $('#myModal').on('shown.bs.modal', function(){
-            $('#myModal .modal-body').html(img);
-        });
-        $('#myModal').on('hidden.bs.modal', function(){
-            $('#myModal .modal-body').html('');
-        });
-   });
-});
+// $(function() {
+//      $('#image_2').on('click',function(){
+//         var src = $(this).attr('src');
+//         var img = '<img src="' + src + '" class="img-responsive"/>';
+//         $('#myModal').modal();
+//         $('#myModal').on('shown.bs.modal', function(){
+//             $('#myModal .modal-body').html(img);
+//         });
+//         $('#myModal').on('hidden.bs.modal', function(){
+//             $('#myModal .modal-body').html('');
+//         });
+//    });
+// });
 
 
 $(function() {
