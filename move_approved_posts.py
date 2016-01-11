16ff937eb6b1457159b2b651089d69c49262b240
Greg Stoddard <stoddardg@gmail.com>
2016-01-11 18:11:32 -0500

    Added script data and a way to load the new information

--- move_approved_posts.py
+++ move_approved_posts.py
@@ -3,10 +3,51 @@ import sys
 
 
 from app import db
-from app.models import Post
+from app.models import Post,ImagePair, Quiz, Quiz_to_ImagePair
 from sqlalchemy.exc import IntegrityError
 import pandas
 from sqlalchemy import create_engine
+
+
+
+
+def import_quiz(filename):
+    for quiz_id, data in quiz_df.groupby('quiz_id'):
+        print quiz_id
+
+        subreddit = data.subreddit.min()
+        num_questions = len(data) / 2
+        #Create a new quiz
+
+        new_quiz = Quiz(subreddit=subreddit, num_questions=10)
+        db.session.add(new_quiz)
+        db.session.commit()
+
+        for pair_id, images in data.groupby('question_id'):
+            image_dict = images.to_dict(orient='records')
+            posts = []
+            for image in image_dict:
+
+                """ First check if the post is in the database already"""
+
+                query_1 = Post.query.filter(Post.reddit_id==image['id'])
+
+                if len(query_1.all()) > 0 :
+                    p = query_1.first()
+                else:
+                    p = Post(image['url'], image['title'], image['score'], image['id'], subreddit, 
+                             image['year'],image['month'])
+                    db.session.add(p)
+                    db.session.commit()
+                posts.append(p)
+            image_pair = ImagePair(image_1_id = posts[0].id, image_2_id = posts[1].id)
+            db.session.add(image_pair)
+            db.session.commit()
+
+
+            quiz_to_image_pair = Quiz_to_ImagePair(quiz_id = new_quiz.id, image_pair_id = image_pair.id)
+            db.session.add(quiz_to_image_pair)
+            db.session.commit()
 
 
 def export_images(file_name):
